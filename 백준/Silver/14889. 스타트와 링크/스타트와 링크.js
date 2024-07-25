0041ef2fe4c7dc5603e50eb05c89d03a935034a4
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-07-25 16:20:18 +0900

    [Silver I] Title: 스타트와 링크, Time: 388 ms, Memory: 12388 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.js
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.js
@@ -6,56 +6,40 @@ const input = require("fs")
     .split("\n");
 
 const N = Number(input.shift());
-const ability = input.map((el) => el.split(" ").map(Number));
+const ability = input.map((line) => line.split(" ").map(Number));
 
-function getCombinations(arr, selectNum) {
-    if (selectNum === 1) return arr.map((el) => [el]);
-
-    const result = [];
-
-    arr.forEach((fixed, idx, origin) => {
-        const rest = origin.slice(idx + 1);
-        const combinations = getCombinations(rest, selectNum - 1);
-        const attached = combinations.map((combination) => [
-            fixed,
-            ...combination,
-        ]);
-
-        result.push(...attached);
-    });
-
-    return result;
-}
-
-function carculateTeamAbility(team) {
-    let result = 0;
-
+function calculateTeamAbility(team) {
+    let sum = 0;
     for (let i = 0; i < team.length; i++) {
         for (let j = 0; j < team.length; j++) {
-            result += ability[team[i]][team[j]];
+            sum += ability[team[i]][team[j]];
         }
     }
-
-    return result;
+    return sum;
 }
-
-const combinations = getCombinations(
-    new Array(N).fill().map((_, idx) => idx),
-    N / 2
-);
 
 let min = Infinity;
 
-for (let i = 0; i < combinations.length; i++) {
-    const linkTeam = new Array(N)
-        .fill()
-        .map((_, idx) => idx)
-        .filter((el) => !combinations[i].includes(el));
+function dfs(index, teamStart) {
+    if (teamStart.length === N / 2) {
+        const teamLink = [];
+        for (let i = 0; i < N; i++) {
+            if (!teamStart.includes(i)) {
+                teamLink.push(i);
+            }
+        }
+        const start = calculateTeamAbility(teamStart);
+        const link = calculateTeamAbility(teamLink);
+        min = Math.min(min, Math.abs(start - link));
+        return;
+    }
 
-    const start = carculateTeamAbility(combinations[i]);
-    const link = carculateTeamAbility(linkTeam);
-
-    min = Math.min(min, Math.abs(start - link));
+    for (let i = index; i < N; i++) {
+        teamStart.push(i);
+        dfs(i + 1, teamStart);
+        teamStart.pop();
+    }
 }
 
+dfs(0, []);
 console.log(min);
