b5553bf771bec6d775c4705dbcdd7d2a27170122
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-08-19 13:16:17 +0900

    [Silver II] Title: 정수 삼각형, Time: 180 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
@@ -5,27 +5,24 @@ const input = require("fs")
     .trim()
     .split("\n");
 
-const n = Number(input.shift());
-const triangle = input.map((line) => line.split(" ").map(Number));
-const dp = Array.from({ length: n }, (_, i) =>
-    Array.from({ length: i + 1 }, () => 0)
-);
+const N = Number(input.shift());
+const arr = input.map((line) => line.split(" ").map(Number));
+const dp = Array.from({ length: N }, (_, i) => Array(i + 1).fill(0));
 
-dp[0][0] = triangle[0][0];
+dp[0][0] = arr[0][0];
 
-for (let i = 1; i < dp.length; i++) {
-    for (let j = 0; j <= i; j++) {
-        if (j === 0) {
-            dp[i][j] = dp[i - 1][j] + triangle[i][j];
-        } else if (j === i) {
-            dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
-        } else {
+for (let i = 1; i < N; i++) {
+    for (let j = 0; j < dp[i].length; j++) {
+        if (j === 0) dp[i][0] = dp[i - 1][0] + arr[i][0];
+        else if (j === dp[i].length - 1)
+            dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
+        else {
             dp[i][j] = Math.max(
-                dp[i - 1][j] + triangle[i][j],
-                dp[i - 1][j - 1] + triangle[i][j]
+                dp[i - 1][j - 1] + arr[i][j],
+                dp[i - 1][j] + arr[i][j]
             );
         }
     }
 }
 
-console.log(Math.max(...dp[n - 1]));
+console.log(Math.max(...dp[N - 1]));
