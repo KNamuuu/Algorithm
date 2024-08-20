937bcc66e86cfbd0371895e9c09b14cac9ede20f
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-08-20 13:58:04 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+const input = require("fs")
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n");
+
+const n = Number(input.shift());
+
+console.log((n * (n - 1)) / 2);
+console.log(2);
