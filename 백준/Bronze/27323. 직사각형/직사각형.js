c1697484a8654acbd78b8365d5a38e987a5491f5
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-08-20 22:43:34 +0900

    [Bronze V] Title: 직사각형, Time: 96 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+const input = require("fs")
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n")
+    .map(Number);
+
+const [A, B] = input;
+
+console.log(A * B);
