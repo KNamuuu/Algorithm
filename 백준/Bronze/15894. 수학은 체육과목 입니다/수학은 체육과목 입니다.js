e95815559a054daa37bbc8de97c7e3baf7752e7d
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-08-21 15:12:34 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.js
@@ -0,0 +1,9 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+const input = require("fs")
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+
+const N = Number(input);
+
+console.log(N * 4);
