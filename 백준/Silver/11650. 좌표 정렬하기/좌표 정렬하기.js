f3c424ca86ea0f6e6887b9d13e5fa4afdc29f488
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-08-21 16:59:52 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 464 ms, Memory: 59648 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -12,6 +12,6 @@ console.log(
         .map((el) => el.split(" ").map(Number))
         .sort((a, b) => a[1] - b[1])
         .sort((a, b) => a[0] - b[0])
-        .map((line) => line.join(" "))
+        .map((el) => el.join(" "))
         .join("\n")
 );
