1ac432e4024c637090c60482eba285a72f4c2a44
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-08-21 17:02:42 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 460 ms, Memory: 60560 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,17 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs")
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n");
+
+const N = Number(input.shift());
+
+console.log(
+    input
+        .map((el) => el.split(" ").map(Number))
+        .sort((a, b) => a[0] - b[0])
+        .sort((a, b) => a[1] - b[1])
+        .map((el) => el.join(" "))
+        .join("\n")
+);
