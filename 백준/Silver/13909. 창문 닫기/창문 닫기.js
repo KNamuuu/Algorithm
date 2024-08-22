6f34cd7b61b2b3ca5bc0b95cfa6a2b1593624809
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-08-22 23:44:05 +0900

    [Silver V] Title: 창문 닫기, Time: 92 ms, Memory: 9576 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs")
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n")
+    .map(Number);
+
+const N = input[0];
+
+console.log(Math.floor(Math.sqrt(N)));
