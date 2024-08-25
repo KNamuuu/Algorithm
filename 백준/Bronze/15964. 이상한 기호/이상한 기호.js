9f5bbe99653b7859289b78b92410e71ab627af25
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-08-25 16:39:10 +0900

    [Bronze V] Title: 이상한 기호, Time: 92 ms, Memory: 9332 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.js
@@ -0,0 +1,9 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs")
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n");
+
+const [N, M] = input.shift().split(" ").map(Number);
+console.log((N + M) * (N - M));
