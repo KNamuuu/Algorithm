adf2cc4b933b0f04869f69b70a5af4221c7f67c4
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-08-28 13:21:31 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 2752 ms, Memory: 12328 KB -BaekjoonHub

---
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.js
@@ -0,0 +1,9 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+const input = require("fs")
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n");
+
+const N = BigInt(input.shift());
+console.log((N % 20000303n).toString());
