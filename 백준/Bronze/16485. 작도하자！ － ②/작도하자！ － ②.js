a0baaab17c3af8d26e6dfd3c79d23d0ea8d91009
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-09-04 16:06:24 +0900

    [Bronze II] Title: 작도하자! - ②, Time: 88 ms, Memory: 9588 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Bronze/16485. 작도하자！ － ②/작도하자！ － ②.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs")
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split(" ")
+    .map(Number);
+
+const [c, b] = input;
+
+console.log(c / b);
