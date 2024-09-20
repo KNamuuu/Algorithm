22797d5dae54d97fbca01f4c5545dab0bf53dc01
Kang DongHa <81519021+KNamuuu@users.noreply.github.com>
2024-09-20 14:49:00 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 120 ms, Memory: 22560 KB -BaekjoonHub

--- 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.js
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.js
@@ -8,19 +8,6 @@ const input = require("fs")
 const answer = [];
 const T = Number(input.shift());
 
-// for (let i = 0; i < T; i++) {
-//     answer.push(`Material Management ${i + 1}`);
-//     const N = Number(input.shift());
-//     const [A, B] = input.shift().split(" ").map(Number);
-
-//     for (let j = 0; j < N; j++) {
-//         const [u, v] = input.shift().split(" ").map(Number);
-//     }
-//     answer.push("Classification ---- End!");
-// }
-
-// console.log(answer.join("\n"));
-
 for (let i = 0; i < T; i++) {
     answer.push(`Material Management ${i + 1}`);
     answer.push("Classification ---- End!");
