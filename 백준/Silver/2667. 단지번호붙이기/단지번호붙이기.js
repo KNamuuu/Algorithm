a6b617b10093d07cbd63a0d92bc9a13bcf0a807f
Dongha Kang <81519021+KNamuuu@users.noreply.github.com>
2025-02-25 22:26:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 156 ms, Memory: 10716 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -1,63 +1,57 @@
-let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-// let input = fs.readFileSync("./input.txt").toString().split("\n");
+const N = +input.shift();
+const map = input.map((line) => line.split("").map(Number));
+const visited = Array.from({ length: N }, () => new Array(N).fill(false));
+const answer = [];
 
-const direction = [
-    [-1, 0],
-    [1, 0],
-    [0, -1],
-    [0, 1],
+const directions = [
+  [0, -1],
+  [0, 1],
+  [-1, 0],
+  [1, 0],
 ];
 
-const N = Number(input.shift());
-const map = input.map((line) => line.split("").map(Number));
+const queue = [];
 
-let block = 0;
-const houses = [];
+function bfs() {
+  let count = 1;
 
-function bfs(x, y) {
-    const queue = [];
+  while (queue.length > 0) {
+    const [x, y] = queue.shift();
 
-    queue.push([x, y, 1]);
-    map[x][y] = 0;
+    for (const [dx, dy] of directions) {
+      const nx = x + dx;
+      const ny = y + dy;
 
-    let answer = 0;
-
-    while (queue.length > 0) {
-        answer++;
-        const [x, y, count] = queue.shift();
-
-        for (const [dx, dy] of direction) {
-            const new_x = x + dx;
-            const new_y = y + dy;
-
-            if (
-                new_x >= 0 &&
-                new_x < N &&
-                new_y >= 0 &&
-                new_y < N &&
-                map[new_x][new_y] === 1
-            ) {
-                queue.push([new_x, new_y, count + 1]);
-                map[new_x][new_y] = 0;
-            }
-        }
+      if (
+        nx >= 0 &&
+        nx < N &&
+        ny >= 0 &&
+        ny < N &&
+        map[nx][ny] === 1 &&
+        !visited[nx][ny]
+      ) {
+        visited[nx][ny] = true;
+        queue.push([nx, ny]);
+        count++;
+      }
     }
+  }
 
-    return answer;
+  return count;
 }
 
 for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-        if (map[i][j] === 1) {
-            block++;
-            houses.push(bfs(i, j));
-        }
+  for (let j = 0; j < N; j++) {
+    if (map[i][j] === 1 && !visited[i][j]) {
+      visited[i][j] = true;
+      queue.push([i, j]);
+      answer.push(bfs());
     }
+  }
 }
 
-houses.sort((a, b) => a - b);
-
-console.log(block);
-console.log(houses.join("\n"));
+console.log(answer.length);
+console.log(answer.sort((a, b) => a - b).join("\n"));
